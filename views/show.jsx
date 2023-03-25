0d090450aa7866676c6f10a308b29e8ba0c4f091
Eshita Islam <islam.eshita@gmail.com>
2023-03-25 11:39:47 -0700

    Adding support for delete and update using helper routes

--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread, index}) {
+function Show ({bread}) {
       return (
         <Default>
             <h3>{bread.name}</h3>
@@ -16,7 +16,7 @@ function Show ({bread, index}) {
             </p>
             <img src={bread.image} alt={bread.name} />
             <li><a href="/breads">Go home</a></li>            
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
             <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">             
               <input type='submit' value="DELETE"/>
             </form>
